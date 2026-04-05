8ce3b8b57b59abbc916f9e268b476046b31d253c
joel-leal <joel.lasi@hotmail.com>
2026-04-05 12:31:31 -0300

    fix: apply review fixes

--- server/src/videos/videos.module.ts
+++ server/src/videos/videos.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { VideosService } from './videos.service';
 import { VideosController } from './videos.controller';
 import { SystemService } from 'src/system.service';
+import { AppLogger } from 'src/core/logging/app-logger.service';
 
 @Module({
   imports: [],
   controllers: [VideosController],
-  providers: [VideosService, SystemService],
+  providers: [VideosService, SystemService, AppLogger],
 })
 export class VideosModule {}
